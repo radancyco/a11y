c415f4874f9557491b94df53dbf55235db3d4c15
Michael Spellacy (Spell) <spell@spellacy.net>
2025-03-10 15:13:25 -0400

    Update init.js

--- pulse/init.js
+++ pulse/init.js
@@ -7,13 +7,13 @@ var btnValidateW3C = document.querySelectorAll(".validate-w3c");
 
 btnValidateWAVE.forEach(function(btn) {
 
-    btn.addEventListener("click", function() {
+    btn.addEventListener("click", function(event) {
+
+        event.preventDefault();
 
         var getLink = btn.getAttribute("href");
-
         window.open(getLink + "report#/" + getLocation, "_blank");
 
-        return false;
 
     });
 
@@ -23,13 +23,12 @@ btnValidateWAVE.forEach(function(btn) {
 
 btnValidateW3C.forEach(function(btn) {
 
-    btn.addEventListener("click", function() {
+    btn.addEventListener("click", function(event) {
+
+        event.preventDefault();
 
         var getLink = btn.getAttribute("href");
-
         window.open(getLink + "?showsource=yes&showoutline=yes&showimagereport=yes&doc=" + getLocation, "_blank");
-
-        return false;
 
     });
 
