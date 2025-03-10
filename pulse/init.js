17e084357ce1cc845616554feb35bfb8038ae032
Michael Spellacy (Spell) <spell@spellacy.net>
2025-03-10 16:24:01 -0400

    Update init.js

--- pulse/init.js
+++ pulse/init.js
@@ -1,54 +1,74 @@
 
-var getLocation = location.href;
+/*!
 
-// WAVE Validation
+  Radancy: Accessibility Pulse
 
-var btnValidateW3C = document.querySelectorAll(".validate-w3c");
+  Contributor(s):
+  Michael "Spell" Spellacy
 
-btnValidateWAVE.forEach(function(btn) {
+*/
 
-    btn.addEventListener("click", function(event) {
+(function() {
 
-        event.preventDefault();
+    "use strict";
+  
+    // Display which Disclosure is in use via console:
+  
+    console.log("%c Accessibility Pulse v1.05 in use. ", "background: #6e00ee; color: #fff");
+  
+    // Commonly used Classes, Data Attributes, States, Strings, etc.
 
-        var getLink = btn.getAttribute("href");
-        window.open(getLink + "report#/" + getLocation, "_blank");
+    let getLocation = location.href;
 
+    // WAVE Validation
 
-    });
+    let btnValidateWAVE = document.querySelectorAll(".validate-wave");
 
-});
+    btnValidateWAVE.forEach(function(btn) {
 
-// W3C Validation
+        btn.addEventListener("click", function(event) {
 
-var btnValidateWAVE = document.querySelectorAll(".validate-wave");
+            event.preventDefault();
 
-btnValidateW3C.forEach(function(btn) {
+            var getLink = btn.getAttribute("href");
+            window.open(getLink + "report#/" + getLocation, "_blank");
 
-    btn.addEventListener("click", function(event) {
-
-        event.preventDefault();
-
-        var getLink = btn.getAttribute("href");
-        window.open(getLink + "?showsource=yes&showoutline=yes&showimagereport=yes&doc=" + getLocation, "_blank");
-
-    });
-
-});
-
-// Heading Validation
-
-var btnValidateHeading = document.querySelectorAll(".validate-heading");
-
-btnValidateHeading.forEach(function(btn) {
-
-    btn.addEventListener("click", function(event) {
-
-        event.preventDefault();
-
-        var getLink = btn.getAttribute("href");
-        window.open(getLink + "?url=" + getLocation, "_blank");
+        });
 
     });
 
-});
+    // W3C Validation
+
+    let btnValidateW3C = document.querySelectorAll(".validate-w3c");
+
+    btnValidateW3C.forEach(function(btn) {
+
+        btn.addEventListener("click", function(event) {
+
+            event.preventDefault();
+
+            var getLink = btn.getAttribute("href");
+            window.open(getLink + "?showsource=yes&showoutline=yes&showimagereport=yes&doc=" + getLocation, "_blank");
+
+        });
+
+    });
+
+    // Heading Validation
+
+    let btnValidateHeading = document.querySelectorAll(".validate-heading");
+
+    btnValidateHeading.forEach(function(btn) {
+
+        btn.addEventListener("click", function(event) {
+
+            event.preventDefault();
+
+            var getLink = btn.getAttribute("href");
+            window.open(getLink + "?url=" + getLocation, "_blank");
+
+        });
+
+    });
+
+})();
