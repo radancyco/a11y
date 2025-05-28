6a8f9b413a86cc7e9e0c763637923ce0a98e44d3
Michael Spellacy (Spell) <spell@spellacy.net>
2025-05-28 18:02:41 -0400

    fgfgfg

--- pulse/component/test/reflow-1280-1024.js
+++ pulse/component/test/reflow-1280-1024.js
@@ -4,15 +4,19 @@ javascript: (function () {
 
     // Ensure the new window has loaded before applying styles
 
-    if (newWindow) {
+    /* 
+
+        There is an issue with setting zoom in Firefox. Page scales differenty than when zooming in with keyboard.
+    
+        if (newWindow) {
   
         newWindow.onload = function () {
         
-           newWindow.document.documentElement.style.zoom = "400%";
+          newWindow.document.documentElement.style.zoom = "400%";
   
         };
 
-    }
+    } */
 
 })();
 
