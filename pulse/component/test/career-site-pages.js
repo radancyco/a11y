d3e458996ca7f8071fcb68a3e0f72125cf5e0031
Michael Spellacy (Spell) <spell@spellacy.net>
2025-07-10 14:32:37 -0400

    Multilingual Support

--- pulse/component/test/career-site-pages.js
+++ pulse/component/test/career-site-pages.js
@@ -12,10 +12,9 @@
     const statusList = shadowContainer.querySelector(".status-container--career-site-pages ul");
     const statusMessage = shadowContainer.querySelector(".status-message--career-site-pages");
     const statusContainerMsg = shadowContainer.querySelector(".status-container__msg");
-    const careerSitePages = document.getElementById("career-site-pages");
     const careerSitePagesLang = document.documentElement.lang;
     
-    statusContainerMsg.textContent = "Loading. Please be pateint. Go make a sandwich. Actually, make me one too. Mmmm sammiches.";
+    statusContainerMsg.textContent = "Fetching pages. Please be pateint. Go make a sandwich. Actually, make me one too. Mmmm sammiches.";
 
     const loadSitemap = async (url) => {
 
@@ -55,11 +54,49 @@
 
             const foldersByLang = {
         
+                "ar": ["الفئة", "الموقع", "التوظبف", "الوظيفة", "موقع-الوظيفة", "البحث-عن-وظائف", "المحتوى", "العمل", "الإحالة"],
+                "bg": ["категория", "местоположение", "наемане-на-работа", "работа", "местоположение-на-работата", "работи-за-търсене", "съдържание", "бизнес", "препращане"],
+                "ca": ["categoria", "ubicació", "ocupació", "treball", "ubicació-del-treball", "cerqueu-feines", "content", "negocis", "derivació"],
+                "zh-hans": ["类别", "位置", "雇用", "工作", "工作地点", "求职", "内容", "公司", "推荐"],
+                "zh-hant": ["類別", "位置", "僱用", "工作", "工作地點", "搜索工作", "內容", "公司", "推薦"],
+                "hr": ["kategorija", "lokacija", "zapošljavanje", "posao", "posao-lokacija", "pretraga-poslova", "sadržaj", "poslovanje", "upućivanje"],
+                "cs": ["kategorie", "místo", "zaměstnanost", "práce", "místo-práce", "vyhledávat-nabídky", "obsah", "faseta", "doporučení"],
+                "da": ["kategori", "placering", "ansaettelse", "job", "jobplacering", "soeg-jobs", "indhold", "virksomhed", "henvisning"],
+                "nl": ["categorie", "plaats", "werk", "banen", "banen-locatie", "jobs-zoeken", "inhoud", "firma", "doorverwijzing"],
+                "en": ["category", "location", "employment", "job", "job_location", "search-jobs", "content", "business", "referral"],
+                "en-gb": ["category", "location", "employment", "job", "job_location", "search-jobs", "content", "business", "referral"],
+                "et": ["kategooria", "asukoht", "teenistus", "tööpakkumine", "töö-asukoht", "otsi-tööpakkumisi", "sisu", "ettevõte", "saatekiri"],
+                "fi": ["kategoria", "sijainti", "tyollistaminen", "tyopaikka", "tyopaikan-sijainti", "hae-tyopaikkoja", "sisalto", "liiketoiminta", "lähetys"],
+                "fr": ["catégorie", "lieu", "emplois", "emploi", "lieu-de-travail", "recherche-d'offres", "contenu", "entreprise", "référence"],
+                "fr-ca": ["catégorie", "lieu", "emplois", "emploi", "lieu-de-travail", "recherche-d'offres", "contenu", "entreprise", "référence"],
                 "de": ["berufsfeld", "länderauswahl", "beschäftigung", "stellenbeschreibung", "arbeitsort", "jobsuche", "inhalt", "firma", "verweisung"],
-                "fr": ["catégorie", "lieu", "emplois", "entreprise", "emploi"],
-                "nl": ["categorie", "plaats", "werk", "banen", "firma"],
-                "pt-br": ["área", "localização", "firma", "vaga", "sub-localização"],
-                "default": ["job", "location", "employment", "category", "business"]
+                "el": ["κατηγορία", "τοποθεσία", "εργασία", "δουλειά", "τοποθεσία-εργασίας", "εργασίες-αναζήτησης", "περιεχόμενο", "επιχείρηση", "παραπομπή"],
+                "he": ["קטגוריה", "מקום", "תעסוקה", "עבודה", "עבודה-מיקום", "חיפוש-משרות", "תוֹכֶן", "עֵסֶק", "הפניה"],
+                "hu": ["kategória", "hely", "foglalkoztatás", "állás", "munkahelye", "álláskeresés", "tartalom", "vállalkozás", "beterjesztés"],
+                "is": ["flokkur", "staður", "ráðning", "starf", "starf-staðsetning", "leit-störf", "efnisinnihald", "viðskipti", "tilvísun"],
+                "id": ["kategori", "lokasi", "hubungan-kerja", "pekerjaan", "lokasi-pekerjaan", "cari-pekerjaan", "konten", "bisnis", "rujukan"],
+                "it": ["categoria", "luogo", "occupazione", "lavoro", "luogo-di-lavoro", "lavori-di-ricerca", "contenuto", "azienda", "rinvio"],
+                "ja": ["カテゴリー", "ロケーション", "雇用", "ジョブ", "仕事の場所", "検索ジョブ", "コンテンツ", "会社", "照会"],
+                "ko": ["범주", "위치", "고용", "직무", "직무-위치", "검색-직무", "내용", "사업", "추천"],
+                "lv": ["kategorija", "atrašanās-vieta", "nodarbinātība", "vakance", "vakances-atrašanās-vieta", "meklēt-darbu", "saturs", "uzņēmums", "nodošana"],
+                "lt": ["kategorija", "vieta", "įdarbinimas", "darbas", "darbo-vieta", "ieškoti-darbų", "turinys", "veikla", "siuntimas"],
+                "ms": ["kategori", "lokasi", "pekerjaan", "kerja", "lokasi-pekerjaan", "cari-pekerjaan", "kandungan", "perniagaan", "rujukan"],
+                "no": ["kategori", "sted", "ansettelse", "jobb", "jobb-sted", "søk-jobber", "innhold", "virksomhet", "henvisning"],
+                "pl": ["kategoria", "lokalizacja", "zatrudnienie", "praca", "miejsce-pracy", "szukanie-pracy", "zawartość", "faseta", "skierowanie"],
+                "pt-pt": ["área", "localização", "emprego", "vaga", "sub-localização", "busca-de-vagas", "conteúdo", "firma", "referência"],
+                "pt-br": ["área", "localização", "emprego", "vaga", "sub-localização", "busca-de-vagas", "conteúdo", "firma", "referência"],
+                "ro": ["categorie", "loc", "angajare", "post-vacant", "loc-post-vacant", "căutare-posturi-vacante", "conținut", "afaceri", "recomandare"],
+                "ru": ["kатегория", "место", "занятость", "pабота", "место-работы", "поиск-вакансий", "cодержание", "бизнес", "направления"],
+                "sr": ["kategorija", "lokacija", "zaposlenje", "posao", "lokacija-posla", "pretraži-poslove", "sadržaj", "poslovanje", "упућивање"],
+                "sk": ["kategória", "umiestnenie", "zamestnanosť", "pracovné-miesto", "umiestnenie-pracovného-miesta", "vyhľadávať-pracovné-miesta", "obsah", "podnik", "odporúčanie"],
+                "sl": ["kategorija", "lokacija", "zaposlitev", "delovno-mesto", "delovno-mesto-lokacija", "iskanje-delovna-mesta", "vsebina", "podjetje", "napotitev"],
+                "es": ["categoría", "ubicación", "empleo", "trabajo", "ciudad-o-región", "buscar-trabajo", "contenido", "empresa", "remisión"],
+                "sv": ["kategori", "plats", "sysselsättning", "jobb", "arbetsplats", "jobbsökande", "innehåll", "firma", "remiss"],
+                "th": ["ประเภท", "สถานที่ตั้ง", "การจ้างงาน", "งาน", "งานสถานที่ตั้ง", "ค้นหาตำแหน่งงาน", "เนื้อหา", "ธุรกิจ", "การอ้างอิง"],
+                "tr": ["kategori", "konum", "işe-alım", "iş", "iş-bulma", "iş-arama", "içerik", "işletme", "referans"],
+                "uk": ["категорія", "місцезнаходження", "зайнятість", "вакансія", "місцезнаходження-вакансії", "пошук-вакансій", "контент", "бізнес", "направлення"],
+                "vi": ["danh-mục", "địa-điểm", "tuyển-dụng", "công-việc", "công-việc-địa-điểm", "tìm-kiếm-công-việc", "nội-dung", "kinh-doanh", "giới-thiệu"],
+                "default": ["category", "location", "employment", "job", "job_location", "search-jobs", "content", "business", "referral"]
             
             };
 
@@ -75,10 +112,48 @@
     
             const jobPathByLang = {
         
-                "de": "stellenbeschreibung",
-                "fr": "emploi",
+                "ar": "الوظيفة",
+                "bg": "работа",
+                "ca": "treball",
+                "zh-hans": "工作",
+                "zh-hant": "工作",
+                "hr": "posao",
+                "cs": "práce",
+                "da": "job",
                 "nl": "banen",
+                "en": "job",
+                "en-gb": "job",
+                "et": "tööpakkumine",
+                "fi": "tyopaikka",
+                "fr": "emploi",
+                "fr-ca": "emploi",
+                "de": "stellenbeschreibung",
+                "el": "δουλειά",
+                "he": "עבודה",
+                "hu": "állás",
+                "is": "starf",
+                "id": "pekerjaan",
+                "it": "lavoro",
+                "ja": "ジョブ",
+                "ko": "직무",
+                "lv": "vakance",
+                "lt": "darbas",
+                "ms": "kerja",
+                "no": "jobb",
+                "pl": "praca",
+                "pt-pt": "vaga",
                 "pt-br": "vaga",
+                "ro": "post-vacant",
+                "ru": "pабота",
+                "sr": "posao",
+                "sk": "pracovné-miesto",
+                "sl": "delovno-mesto",
+                "es": "trabajo",
+                "sv": "jobb",
+                "th": "งาน",
+                "tr": "iş",
+                "uk": "вакансія",
+                "vi": "công-việc",
                 "default": "job"
     
             };
